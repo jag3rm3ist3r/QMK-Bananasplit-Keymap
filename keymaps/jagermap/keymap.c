26cf071ff7c052502e1705ecb2e6456eb71b6804
jag3rm3ist3r <jager.mh@gmail.com>
2022-01-18 05:41:17 +1100

    removed esc function from middle function key cause it was annoying

--- keymaps/jagermap/keymap.c
+++ keymaps/jagermap/keymap.c
@@ -941,7 +941,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,  TD(TD_LCOM),  TD(TD_RCOM), KC_BSPC, \
     MO(LAYER_1),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,   KC_QUOT,  KC_ENT, \
     KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT, KC_LCAP, \
-    KC_LCTL,  LSUP,  KC_LALT,		KC_ENT,		LT(LAYER_2, KC_ESC),	KC_SPC,		KC_RALT, RSUP, KC_NO, KC_APP, KC_RCTL \
+    KC_LCTL,  LSUP,  KC_LALT,		KC_ENT,		MO(LAYER_2),	KC_SPC,		KC_RALT, RSUP, KC_NO, KC_APP, KC_RCTL \
   ),
 
   [LAYER_1] = LAYOUT_jagermap(
